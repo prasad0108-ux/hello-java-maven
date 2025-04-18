c274785aab00c8ac15d9e88c06bec21bc12dc7ea
prasad0108-ux <suryavanshipr30@gmail.com>
2025-04-18 21:43:39 +0530

    Create HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
+    }
+
+    public static String getMessage() {
+        return "Hello from getMessage()!";
+    }
+}
